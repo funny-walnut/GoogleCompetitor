b01f8b50dee939fe0f33b0ca22432dfa648cc80d
funny-walnut <59824970+funny-walnut@users.noreply.github.com>
2020-12-18 14:27:01 +0300

    Update lessons.cs

--- lessons.cs
+++ lessons.cs
@@ -7,77 +7,96 @@ namespace Level1Space
   {
       public static int [] WordSearch(int len, string s, string subs)
         {
-            string[] words = s.Split(' '); //делим строку на слова по пробелам
-
-            string[] gotov = new string[words.Length]; //массив строк , на которые разделим большую строку по заданной ширине
+            int a = 0; //счетчик пробелов
             int counterString = 0; //счетчик строк в массиве
-            string summary = null; // временная строка до присвоения в массив строк
-            string medium = null; 
-            for (int i = 0; i < words.Length - 1; i++)
+            string[] gotov = new string[s.Length]; //массив строк , на которые разделим большую строку по заданной ширине
+            for (int i = 0; i < s.Length;i++)
             {
-                if (summary == null)
-                    summary = words[i];
-           
-                if (summary.Length < len)
+                if (Char.IsWhiteSpace(s,i))
                 {
-                    summary = summary + " "; //перед прибавлением следующего слова прибавляем пробел
+                    a++;
+                }
+            }
+            if (a != 0)
+            {
+                string[] words = s.Split(' '); //делим строку на слова по пробелам
+
+
+
+                string summary = null; // временная строка до присвоения в массив строк
+                string medium = null;
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    if (summary == null)
+                        summary = words[i];
+
                     if (summary.Length < len)
                     {
-                        medium = summary + words[i + 1];//прибавляем следующее слово и проверяем, умещается ли оно по заанной ширине
-                        
-                        if (medium.Length > len)
+                        summary = summary + " "; //перед прибавлением следующего слова прибавляем пробел
+                        if (summary.Length < len)
                         {
-                            gotov[counterString] = summary; //если не умещается, убираем прибавленное слово
-                            summary = words[i + 1];
-                            counterString++;
+                            medium = summary + words[i + 1];//прибавляем следующее слово и проверяем, умещается ли оно по заанной ширине
+
+                            if (medium.Length > len)
+                            {
+                                gotov[counterString] = summary; //если не умещается, убираем прибавленное слово
+                                summary = words[i + 1];
+                                counterString++;
+                            }
+                            else
+                            {
+                                summary = medium;
+                            }
+
                         }
                         else
                         {
-                            summary = medium;
+                            gotov[counterString] = summary;
+                            summary = null;
+                            counterString++;
                         }
 
                     }
-                    else {
+                    else if (summary.Length == len)
+                    {
                         gotov[counterString] = summary;
                         summary = null;
                         counterString++;
                     }
+                    else
+                    {
+                        gotov[counterString] = summary.Substring(0, 12);
+                        summary = summary.Substring(12, summary.Length - 12);
+                        i--;
+                        counterString++;
+                    }
+                }
+                // разбираемся с последним словом
+                if (summary == null)
+                    summary = words[words.Length - 1];
+                while (summary != null)
+                {
+                    if (summary.Length <= len)
+                    {
+                        gotov[counterString] = summary;
+                        summary = null;
+                        counterString++;
+                    }
+                    else
+                    {
+                        gotov[counterString] = summary.Substring(0, 12);
+                        summary = summary.Substring(12, summary.Length - 12);
+                        counterString++;
+                    }
 
                 }
-                else if (summary.Length == len)
-                {
-                    gotov[counterString] = summary;
-                    summary = null;
-                    counterString++;
-                }
-                else
-                {
-                    gotov[counterString] = summary.Substring(0, 12);
-                    summary = summary.Substring(12, summary.Length - 12);
-                    i--;
-                    counterString++;
-                }
             }
-            // разбираемся с последним словом
-            if (summary == null)
-                summary = words[words.Length - 1];
-            while (summary != null)
+
+            else
             {
-                if (summary.Length <= len)
-                {
-                    gotov[counterString] = summary;
-                    summary = null;
-                    counterString++;
-                }
-                else
-                {
-                    gotov[counterString] = summary.Substring(0, 12);
-                    summary = summary.Substring(12, summary.Length - 12);
-                    counterString++;
-                }
-
+                counterString = 1;
+                gotov[0] = s;
             }
-        
             int[] wordFounder = new int[counterString ];//массив чисел указывающих, обнаружено ли слово
             for (int i = 0; i < counterString; i++)
             {
